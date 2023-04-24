398dc43589498dbf6789a0c29bb7c033da57ab27
jmarcossss <joaomalcantaraaa@gmail.com>
2023-04-24 01:01:01 -0300

    feat(service): adding hotels service

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,21 +1,35 @@
 import { notFoundError } from '@/errors';
 import hotelRepository from '@/repositories/hotels-repository';
 
-export async function findAllHotels() {
+async function findHotels(filter?: (hotel: any) => boolean) {
   const hotels = await hotelRepository.findAllHotels();
-  if (hotels.length === 0) throw notFoundError();
+
+  if (hotels.length === 0) {
+    throw notFoundError();
+  }
+
+  if (filter) {
+    const filteredHotels = hotels.filter(filter);
+
+    if (filteredHotels.length === 0) {
+      throw notFoundError();
+    }
+
+    return filteredHotels;
+  }
+
   return hotels;
 }
 
+export async function findAllHotels() {
+  return findHotels();
+}
+
 export async function findHotelById(id: number) {
-  const hotel = await hotelRepository.findHotelById(id);
-  if (!hotel) throw notFoundError();
+  const [hotel] = await findHotels((h) => h.id === id);
   return hotel;
 }
 
-const hotelService = {
-  findAllHotels,
-  findHotelById,
-};
+const hotelService = { findAllHotels, findHotelById };
 
 export default hotelService;
