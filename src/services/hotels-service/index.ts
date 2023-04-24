894d11fed1daf6b23964b343b8197d908527728f
jmarcossss <joaomalcantaraaa@gmail.com>
2023-04-24 01:40:28 -0300

    fix(services): fixing hotels service error

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,32 +1,16 @@
 import { notFoundError } from '@/errors';
 import hotelRepository from '@/repositories/hotels-repository';
 
-async function getFilteredHotels(filter?: (hotel: any) => boolean): Promise<any[]> {
-  const hotels = await hotelRepository.findAllHotels();
-
-  if (!filter) {
-    return hotels;
-  }
-
-  return hotels.filter(filter);
-}
-
-function throwIfEmpty(hotels: any[]) {
-  if (hotels.length === 0) {
-    throw notFoundError();
-  }
-}
-
 export async function findAllHotels() {
-  const hotels = await getFilteredHotels();
-  throwIfEmpty(hotels);
+  const hotels = await hotelRepository.findAllHotels();
+  if (hotels.length === 0) throw notFoundError();
   return hotels;
 }
 
 export async function findHotelById(id: number) {
-  const filteredHotels = await getFilteredHotels((h) => h.id === id);
-  throwIfEmpty(filteredHotels);
-  return filteredHotels[0];
+  const hotel = await hotelRepository.findHotelById(id);
+  if (!hotel) throw notFoundError();
+  return hotel;
 }
 
 const hotelService = {
